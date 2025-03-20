f62e03a38b63ab8e32bb50d576735a35ffcbbee8
Whitherhs <108372102+Whitherhs@users.noreply.github.com>
2025-03-20 00:57:21 +0000

    LAB 2
    começo
    
    dia 19/03

--- compiler/compiler.h
+++ compiler/compiler.h
@@ -4,6 +4,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*================== BEGIN LAB 2 =============================================*/
+#include <stdbool.h>
+
+struct pos{
+    int line;
+    int col;
+    const char* filename;
+};
+
+enum {
+    LEXICAL_ANALYSYS_ALL_OK ,
+    LEXICAL_ANALYSYS_IMPUT_ERROR 
+};
+
+enum{
+    TOKEN_TYPE_KEYWORD ,
+    TOKEN_TYPE_IDENTIFIER ,
+    TOKEN_TYPE_OPERATOR ,
+    TOKEN_TYPE_SYMBOL ,
+    TOKEN_TYPE_NUMBER ,
+    TOKEN_TYPE_STRING ,
+    TOKEN_TYPE_COMMENT ,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+
+    union{
+        char cval;
+        const char *sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+    
+    // Sera 'true' se tiver um espaço entre um token e o próximo token.
+    bool whitespace;
+
+    // Retira a string que estiver dentro de parênteses. ex (1+2+3) resulta em 1+2+3.
+    const char* between_brackets;
+};
+
+struct lex_process;
+
+// Definição de ponteiros para funções.
+typedef char (*LEX_PROCESS_NEXT_CHAR) (struct lex_process* process);
+typedef char (*LEX_PROCESS_PEAK_CHAR) (struct lex_process* process);
+typedef char (*LEX_PROCESS_PUSH_CHAR) (struct lex_process* process);
+
+struct lex_process_functions {
+    LEX_PROCESS_NEXT_CHAR next_char;
+    LEX_PROCESS_PEAK_CHAR peak_char;
+    LEX_PROCESS_PUSH_CHAR push_char;
+};
+
+struct lex_process {
+    struct pos pos;
+    struct vector* token_vec;
+    struct compile_process* compiler;
+
+    int current_expression_count; // Qts parenteses existem no momento.
+
+    struct buffer* parentheses_buffer;
+    struct lex_process_functions* function;
+
+    void* private; //Dados privados que o lexer nao entende mas o programador entende.
+};
+
+// Funções do arquivo cprocess.c
+char compile_process_next_char(struct lex_process* lex_process);
+char compile_process_peek_char(struct lex_process* lex_process);
+void compile_process_push_char(struct lex_process* lex_process, char c);
+
+// Funções do arquivo lex_process.c
+struct lex_process* lex_process_create(struct compile_process* compiler, struct lex_process_functions* functions, void *private);
+void lex_process_free(struct lex_process* process);
+void* lex_process_private(struct lex_process* process);
+struct vector* lex_process_tokens(struct lex_process* process);
+
+//Funcoes do arquivo lexer.c
+int lex(struct lex_process* process);
+
+/*================== END LAB 2 ===============================================*/
+
 enum
 {
     COMPILER_FILE_COMPILED_OK,
@@ -14,6 +102,9 @@ struct compile_process
 {
     //como o arquivo deve ser compilado
     int flags;
+
+    /* LAB 2 */
+    struct pos;
 
     struct compile_prcess_imput_file
     {
