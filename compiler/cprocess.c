96b83781964e9852a050efef3d4845bc31ec15e4
Brunna Ruas <146996546+brunnaruass@users.noreply.github.com>
2025-03-26 14:03:36 -0300

    Update cprocess.c LAB 2 adicionar do slide

--- compiler/cprocess.c
+++ compiler/cprocess.c
@@ -1,5 +1,33 @@
 #include "compiler.h"
 
+/* BEGIN – LAB 2 ---------------------------------------------*/
+// Pega um caracter do arquivo e atualiza a posicao.
+char compile_process_next_char(struct lex_process* lex_process) {
+    struct compile_process* compiler = lex_process->compiler;
+    compiler->pos.col += 1;          // Atualiza a posicao da coluna.
+    char c = getc(compiler->cfile.fp);
+    if (c == '\n') {
+        compiler->pos.line += 1;
+        compiler->pos.col = 1;
+    }
+    return c;
+}
+
+// Apenas pega um caracter do arquivo.
+char compile_process_peek_char(struct lex_process* lex_process) {
+    struct compile_process* compiler = lex_process->compiler;
+    char c = getc(compiler->cfile.fp);
+    ungetc(c, compiler->cfile.fp);
+    return c;
+}
+
+// Adicionar um caracter no arquivo.
+void compile_process_push_char(struct lex_process* lex_process, char c) {
+    struct compile_process* compiler = lex_process->compiler;
+    ungetc(c, compiler->cfile.fp);
+}
+/* END – LAB 2 -----------------------------------------------*/
+
 struct compile_process* compile_process_create(const char* filename, const char* filename_out, int flags){
     FILE *file = fopen(filename, "r");
 
