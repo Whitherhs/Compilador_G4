1719dfb5d1e276b62a948bdbbadf4ff96c99398b
Whitherhs <108372102+Whitherhs@users.noreply.github.com>
2025-04-03 00:15:07 +0000

    Update compiler.h

--- compiler/compiler.h
+++ compiler/compiler.h
@@ -1,23 +1,41 @@
 #ifndef COMPILER_H
 #define COMPILER_H
 
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 
-/*================== BEGIN LAB 2 =============================================*/
+/* BEGIN - LAB 2 ---------------------------------*/
 #include <stdbool.h>
+
+struct pos {
+    int line;
+    int col;
+    const char* filename;
+};
+
 #define NUMERIC_CASE \
-    case '0': \
-    case '1': \
-    case '2': \
-    case '3': \
-    case '4': \
-    case '5': \
-    case '6': \
-    case '7': \
-    case '8': \
+    case '0':   \
+    case '1':   \
+    case '2':   \
+    case '3':   \
+    case '4':   \
+    case '5':   \
+    case '6':   \
+    case '7':   \
+    case '8':   \
     case '9'
 
+#define SYMBOL_CASE \
+    case '{':   \
+    case '}':   \
+    case ':':   \
+    case ';':   \
+    case '#':   \
+    case ']':   \
+    case '\\':  \
+    case ')'
+
+/*
 #define KEYWORD_CASE \
     case 'unsigned': \
     case 'signed':   \
@@ -49,20 +67,14 @@
     case 'const':    \
     case 'extern':   \
     case 'restrict'
-
-
-struct pos{
-    int line;
-    int col;
-    const char* filename;
-};
+*/
 
 enum {
     LEXICAL_ANALYSIS_ALL_OK ,
-    LEXICAL_ANALYSIS_IMPUT_ERROR 
+    LEXICAL_ANALYSIS_IMPUT_ERROR
 };
 
-enum{
+enum {
     TOKEN_TYPE_KEYWORD ,
     TOKEN_TYPE_IDENTIFIER ,
     TOKEN_TYPE_OPERATOR ,
@@ -73,34 +85,33 @@ enum{
     TOKEN_TYPE_NEWLINE
 };
 
-struct token
-{
+struct token{ 
     int type;
     int flags;
 
-    struct pos pos;//Identificar onde o token esta no arquivo.
+    struct pos pos; //Identificar onde o token esta no arquivo.
 
-    union{
+    union {
         char cval;
         const char *sval;
         unsigned int inum;
         unsigned long lnum;
         unsigned long long llnum;
         void* any;
+
     };
-    
     // Sera 'true' se tiver um espaço entre um token e o próximo token.
     bool whitespace;
 
-    // Retira a string que estiver dentro de parênteses. ex (1+2+3) resulta em 1+2+3.
+    // Retira a string que estiver dentro de parênteses. Ex: (1+2+3) resulta em 1+2+3.
     const char* between_brackets;
 };
 
 struct lex_process;
 
-// Definição de ponteiros para funções.
-typedef char (*LEX_PROCESS_NEXT_CHAR) (struct lex_process* process);
-typedef char (*LEX_PROCESS_PEEK_CHAR) (struct lex_process* process);
+// Definicao de ponteiros para funcoes.
+typedef char (*LEX_PROCESS_NEXT_CHAR) (struct lex_process* process);            
+typedef char (*LEX_PROCESS_PEEK_CHAR) (struct lex_process* process);            
 typedef void (*LEX_PROCESS_PUSH_CHAR) (struct lex_process* process, char c);
 
 struct lex_process_functions {
@@ -114,7 +125,8 @@ struct lex_process {
     struct vector* token_vec;
     struct compile_process* compiler;
 
-    int current_expression_count; // Qts parenteses existem no momento.
+    
+    int current_expression_count; //Qts parenteses existem no momento.
 
     struct buffer* parentheses_buffer;
     struct lex_process_functions* function;
@@ -122,44 +134,47 @@ struct lex_process {
     void* private; //Dados privados que o lexer nao entende mas o programador entende.
 };
 
-// Funções do arquivo cprocess.c
+// Funcoes do arquivo cprocess.c
 char compile_process_next_char(struct lex_process* lex_process);
 char compile_process_peek_char(struct lex_process* lex_process);
 void compile_process_push_char(struct lex_process* lex_process, char c);
 
-// Funções do arquivo lex_process.c
+// Funcoes do arquivo lex_process.c
 struct lex_process* lex_process_create(struct compile_process* compiler, struct lex_process_functions* functions, void *private);
 void lex_process_free(struct lex_process* process);
 void* lex_process_private(struct lex_process* process);
 struct vector* lex_process_tokens(struct lex_process* process);
 
-//Funcoes do arquivo lexer.c
+// Funcoes do arquivo lexer.c
 int lex(struct lex_process* process);
 
-/*================== END LAB 2 ===============================================*/
+// Funcoes do arquivo compiler.c
+void compiler_error(struct compile_process* compiler, const char* msg, ...);
+void compiler_warning(struct compile_process* compiler, const char* msg, ...);
 
-enum
-{
+
+/* END - LAB 2 ---------------------------------*/
+
+enum{
     COMPILER_FILE_COMPILED_OK,
     COMPILER_FAILED_WITH_ERRORS
 };
 
-struct compile_process 
-{
-    //como o arquivo deve ser compilado
+struct compile_process {
+    // Como o arquivo deve ser compilado
     int flags;
 
-    /* LAB 2 */
+    /* LAB2*/
     struct pos pos;
 
-    struct compile_prcess_imput_file
-    {
+    struct compile_process_imput_file{
         FILE* fp;
         const char* abs_path;
     } cfile;
 
-    FILE* ofile;  
+    FILE* ofile;
 };
+
 
 int compile_file(const char* filename, const char* out_finename, int flags);
 struct compile_process* compile_process_create(const char* filename, const char* filename_out, int flags);
