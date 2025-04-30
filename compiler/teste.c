1ec828ae80c0ff207b498f457d4b49bd70e3b9f5
Whitherhs <108372102+Whitherhs@users.noreply.github.com>
2025-04-30 22:35:15 +0000

    Update teste.c

--- compiler/teste.c
+++ compiler/teste.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+
+// Teste de Comentario
+
+int main(){
+    printf("hello World");
+
+    return 0;
+}
+
+
+/* Teste de Comentario */
+
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    float b = 2;
+    double c = 3;
+
+
+    return a + b * c;
+}
+#include <stdio.h>
 #include <stdlib.h>
 
-void teste(){
-    printf("teste");
+int main(){
+    int a = 1;
+    float b = 2;
+    double c = 3;
+
+    // teste de comentario
+
+    return (a + b) / c;
 }
