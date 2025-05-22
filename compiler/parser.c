915d19a37b26f8730fd391aeb482cc34b832582f
Whitherhs <108372102+Whitherhs@users.noreply.github.com>
2025-05-22 00:25:26 +0000

    Update parser.c

--- compiler/parser.c
+++ compiler/parser.c
@@ -1,5 +1,6 @@
 #include "compiler.h"
 #include "helpers/vector.h"
+#include <assert.h> // LAB4
 
 static struct compile_process* current_process;
 static struct token* parser_last_token;
@@ -8,6 +9,9 @@ static struct token* parser_last_token;
 struct history {
     int flags;
 };
+
+static bool parser_left_op_has_priority(const char* op_left, const char* op_right); // LAB4
+extern struct expressionable_op_precedence_group op_precedence[TOTAL_OPERADOR_GROUPS]; // LAB4
 
 void parse_expressionable(struct history* history);
 int parse_expressionable_single(struct history* history);
@@ -165,4 +169,81 @@ int parse(struct compile_process* process) { /*LAB3: Adicionar o prototipo no co
     }
     
     return PARSE_ALL_OK;
+}
+
+void parser_node_shift_children_left(struct node* node) {
+    assert(node->type == NODE_TYPE_EXPRESSION);
+    assert(node->exp.right->type == NODE_TYPE_EXPRESSION);
+
+    const char* right_op = node->exp.right->exp.op;
+    struct node* new_exp_left_node = node->exp.left;
+    struct node* new_exp_right_node = node->exp.right->exp.left;
+    make_exp_node(new_exp_left_node, new_exp_right_node, node->exp.op);
+
+    // EX: 50*E(20+50) -> E(50*20)+50
+    struct node* new_left_operand = node_pop();
+    struct node* new_right_operand = node->exp.right->exp.right;
+    node->exp.left = new_left_operand;
+    node->exp.right = new_right_operand;
+    node->exp.op = right_op;
+}
+
+void parser_reorder_expression(struct node** node_out) {
+    struct node* node = *node_out;
+
+    // Se o node nao for do tipo expressao, finalizar.
+    if (node->type != NODE_TYPE_EXPRESSION) return;
+
+    // Se o node nao tiver filhos que sejam expressoes, finalizar.
+    if (node->exp.left->type != NODE_TYPE_EXPRESSION && node->exp.right && node->exp.right->type != NODE_TYPE_EXPRESSION) return;
+
+    if (node->exp.left->type != NODE_TYPE_EXPRESSION && node->exp.right && node->exp.right->type == NODE_TYPE_EXPRESSION) {
+        const char* op = node->exp.right->exp.op;
+        const char* right_op = node->exp.right->exp.op;
+
+        if (parser_left_op_has_priority(node->exp.op, right_op)) {
+        
+            // EX: 50*E(20+50) -> E(50*20)+50
+            parser_node_shift_children_left(node);
+
+            // Reordenar a arvore depois do shift ser realizado.
+            parser_reorder_expression(&node->exp.left);
+            parser_reorder_expression(&node->exp.right);
+        }
+    }
+} 
+
+static int parser_get_precedence_for_operator(const char* op, struct expressionable_op_precedence_group** group_out) {
+    *group_out = NULL;
+    for (int i = 0; i < TOTAL_OPERADOR_GROUPS; i++) {
+        for (int j = 0; op_precedence[i].operators[j]; j++) {
+            const char* _op = op_precedence[i].operators[j];
+            if (S_EQ(op, _op)) {
+                *group_out = &op_precedence[i];
+                return i;
+            }
+        }
+    }
+
+    return -1;
+}
+
+void print_tree(struct  node *node)
+{
+    /* data */
+}
+
+static bool parser_left_op_has_priority(const char* op_left, const char* op_right) {
+    struct expressionable_op_precedence_group* group_left = NULL;
+    struct expressionable_op_precedence_group* group_right = NULL;
+
+    // Se os operadores forem os mesmos, retornar falso.
+    if (S_EQ(op_left, op_right)) return false;
+    int precedence_left = parser_get_precedence_for_operator(op_left, &group_left);
+    int precedence_right = parser_get_precedence_for_operator(op_right, &group_right);
+
+    // Essa funcao so trata de associatividade esquerda para direita
+    if (group_left->associativity == ASSOCIATIVITY_RIGHT_TO_LEFT) return false;
+
+    return precedence_left <= precedence_right;
 }
